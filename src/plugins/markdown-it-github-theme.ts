450583d9b8610911fd3b8590aa9f23a161b5d76f
0x219 <lzm0x219@gmail.com>
2022-12-11 03:07:36 +0800

    Update code styles

--- src/plugins/markdown-it-github-theme.ts
+++ src/plugins/markdown-it-github-theme.ts
@@ -2,9 +2,9 @@ export default function markdownItGithubTheme(md: markdownit) {
 	const render = md.renderer.render;
 	md.renderer.render = function (...args) {
 		return `
-    <div class="vscode-markdown-github">
-      ${render.apply(md.renderer, args)}
-    </div>
+      <div class="vscode-markdown-github">
+        ${render.apply(md.renderer, args)}
+      </div>
     `;
 	};
 	return md;
