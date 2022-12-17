b61b22312aacae4aaf60cde012f44d44b61d258f
0x219 <lzm0x219@gmail.com>
2022-12-17 17:30:45 +0800

    Add configuration compatible with v1 version

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,5 +3,13 @@ import vscode from "vscode";
 export const configurationSection = "vscode-markdown-github";
 
 export function getConfiguration() {
-	return vscode.workspace.getConfiguration(configurationSection);
+	const configuration = vscode.workspace.getConfiguration(configurationSection);
+
+	// Compatible with v1 version
+	const theme = configuration.get("theme");
+	if (typeof theme === "string") {
+		configuration.update("theme", undefined);
+	}
+
+	return configuration;
 }
