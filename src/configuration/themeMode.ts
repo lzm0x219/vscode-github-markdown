edd4826a5a2d0e11b0925ef2bcf4a89cbc1bc468
0x219 <lzm0x219@gmail.com>
2023-04-27 14:24:50 +0800

    Remove events folder

--- src/configuration/themeMode.ts
+++ src/configuration/themeMode.ts
@@ -1,23 +1,23 @@
 import { getConfiguration } from "../configuration";
 
-export type ThemeMode = typeof themeMode[number];
+export type ThemeMode = (typeof themeMode)[number];
 
 export const themeMode = ["light", "dark", "auto"] as const;
 
 export const themeModeAlias: Record<ThemeMode, string> = {
-	light: "Light",
-	dark: "Dark",
-	auto: "Sync to the system",
+  light: "Light",
+  dark: "Dark",
+  auto: "Sync to the system",
 };
 
 export const themeModeSection = "theme.mode";
 
 export function getThemeMode() {
-	const configuration = getConfiguration();
-	return configuration.get<ThemeMode>(themeModeSection);
+  const configuration = getConfiguration();
+  return configuration.get<ThemeMode>(themeModeSection);
 }
 
 export async function setThemeMode(mode: ThemeMode) {
-	const configuration = getConfiguration();
-	await configuration.update(themeModeSection, mode, true);
+  const configuration = getConfiguration();
+  await configuration.update(themeModeSection, mode, true);
 }
