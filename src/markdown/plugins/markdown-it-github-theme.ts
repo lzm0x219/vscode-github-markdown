a3ecd3a4ee404b43b257c1aa742496f2d9f96e57
0x219 <lzm0x219@gmail.com>
2023-12-03 16:14:10 +0800

    Wip

--- src/markdown/plugins/markdown-it-github-theme.ts
+++ src/markdown/plugins/markdown-it-github-theme.ts
@@ -4,23 +4,11 @@ import { ThemeColorMode, ThemeMode } from "../constants";
 export default function markdownItGitHubTheme(md: markdownit) {
   const render = md.renderer.render;
 
-  const getColorMode = (): ThemeColorMode => {
-    const mode = configuration.getThemeMode();
-    const theme = configuration.getThemeSingle();
-    if (mode === ThemeMode.Single) {
-      if (theme.includes("light")) {
-        return ThemeColorMode.Light;
-      }
-      return ThemeColorMode.Dark;
-    }
-    return ThemeColorMode.Auto;
-  };
-
   md.renderer.render = function (...args) {
     return `
       <div
         class="vscode-github-markdown"
-        data-color-mode="${getColorMode()}"
+        data-color-mode="${1}"
         data-light-theme="${configuration.getThemeSystemDay()}"
         data-dark-theme="${configuration.getThemeSystemNight()}"
       >
